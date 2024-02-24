cef8790a613541b1924d30688e7570811ad83d7d
Yulia Chirkova <yu.ya98@ya.ru>
2024-02-24 12:24:22 +0300

    5 task of module 1 was done

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,9 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  let length = str.length;
+
+  if (length <= maxlength) {
+    return str;
+  } else {
+    return str.substr(0, maxlength - 1) + '…';
+  }
 }
