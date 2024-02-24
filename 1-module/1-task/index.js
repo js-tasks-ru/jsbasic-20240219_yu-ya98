13bfaf6d17cf754df3dced107ba8b0d9e886ee5b
Yulia Chirkova <yu.ya98@ya.ru>
2024-02-24 11:28:10 +0300

    task 1 of module 1 was done

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  if (n > 1) {
+    let result = n; 
+    for (let i = n; i >= 2; i--) {
+      result *= (i - 1);
+    }
+    return result;
+  } else if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return 'Неправильное значение';
+  }
 }
