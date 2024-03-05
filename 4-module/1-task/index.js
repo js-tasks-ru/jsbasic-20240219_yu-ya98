395b147c6ebafbc5a101f9e7036b6d947473e6fa
Yulia Chirkova <yu.ya98@ya.ru>
2024-03-05 19:59:23 +0300

    1 task of module 4 was done

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+
+  for (let friend of friends) {
+    ul.insertAdjacentHTML('beforeend', `<li>${friend.firstName} ${friend.lastName}</li>`);
+  }
+  
+  return ul;
 }
