1932ba58924abfaf72314c194777cd0ccda1db12
Yulia Chirkova <yu.ya98@ya.ru>
2024-03-15 11:44:18 +0300

    1 task of module 6 was done

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,30 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.render();
+    this.addEventListeners();
+  }
+
+  render() {
+    let table = document.createElement('table');
+    table.innerHTML = '<thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead><tbody></tbody>';
+
+    let tBody = table.tBodies[0];
+    tBody.innerHTML = this.rows
+      .map(({name, age, salary, city}) => `<tr><td>${name}</td><td>${age}</td><td>${salary}</td><td>${city}</td><td><button>X</button></td></tr>`)
+      .join('');
+
+    this.elem = table;
+  }
+
+  addEventListeners() {
+    this.elem.addEventListener('click', event => {
+      let btn = event.target.closest('button');
+
+      if (btn) {
+        btn.closest('tr').remove();
+      }
+    });
   }
 }
